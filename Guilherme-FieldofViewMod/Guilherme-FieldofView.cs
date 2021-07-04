4e0e26f3e7cd89a5dafd580714da0ce03077b213
source-guilherme <guilherme_goi@hotmail.com>
2021-07-03 22:39:06 -0400

    Update files

---
+++ Guilherme-FieldofViewMod/Guilherme-FieldofView.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using Jotunn.Utils;
+
+namespace FieldofViewMod
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class FieldofViewMod : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.source-guilherme.fieldofviewmod";
+        public const string PluginName = "FieldofViewMod";
+        public const string PluginVersion = "0.0.1";
+        private ConfigEntry<float> FloatConfig;
+
+        private void Awake()
+        {
+            FloatConfig = Config.Bind<float>("Field of View Mod", "Field of View Values", 65f, new ConfigDescription("This will change the Field of View in the game.", new AcceptableValueRange<float>(0f, 180f)));
+            On.GameCamera.UpdateCamera += ChangeFOV;
+            Jotunn.Logger.LogInfo("Field of View has loaded!");
+        }
+
+        private void ChangeFOV(On.GameCamera.orig_UpdateCamera orig, GameCamera self, float dt)
+        {
+            self.m_fov = FloatConfig.Value;
+            orig(self, dt);
+        }
+    }
+}
---
+++ Guilherme-FieldofViewMod/Package/README.md
@@ -0,0 +1,33 @@
+﻿# FieldofViewMod
+This mod allows you to change the Field of View on Camera Player.
+
+## Installation (manual)
+Drop the folder or .dll file on Valheim\BepInEx\plugins
+
+It will be:
+Valheim\BepInEx\plugins\FieldofViewMod\FieldofViewMod.dll
+or
+Valheim\BepInEx\plugins\FieldofViewMod.dll
+
+## Features
+On Menu:
+To change the values, go to:
+Settings
+ModConfig
+Find the section for Field of View Mod
+Type any value ranging 0 to 180, default values is 65
+Press Ok
+
+In Game:
+Press ESC
+Settings
+ModConfig
+Find the section for Field of View Mod
+Type any value ranging 0 to 180, default values is 65
+Press Ok and the change is immediate.
+
+## Changelog
+0.0.1 - First Launch
+
+## Known issues
+You can find the github at: https://github.com/source-guilherme/friendly-barnacle
